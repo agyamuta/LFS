1894ca64afcd3a88013c2cd52be0486b8de1afbf
root <root@ursa.com>
2017-08-15 18:24:33 -0700

    discard

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-Test for LFS #1
-
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf145963e067422336a85cb41aa287a9bc40ae0c85108bdfac6c1775f6fc764e
-size 1008
+oid sha256:af45e786b9bdbbad6a64a94e89659d0ff22315dab3ec85e5b2f855b4ae48eeb9
+size 146
